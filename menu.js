2fd3447d5dee7e8bae2d5fc3a1d2d5f5b6f68b71
tobiaspossetto <tobigpossetto@gmail.com>
2021-01-30 16:17:11 -0300

    inicio terminado bem+sass+js

--- menu.js
+++ menu.js
@@ -2,22 +2,34 @@ $(document).ready(main);
 
 var contador = 1;
 
+function cambiarImagenjQ() {
+    if (contador == 1) {
+
+        $("#icono").attr("src", "assets/img/menu-activo.png");
+        $("#icono").attr("width", "25px");
+    } else {
+        $("#icono").attr("src", "assets/img/menu.png");
+    }
+}
+
 function main() {
-    $('.bt-menu').click(function() {
+    $('.menu-responsive__bt-menu').click(function() {
         //$('nav').toggle();
         if (contador == 1) {
-            $(".contenedor-boton").css("background-color", "#FFB40D");
+            //$(".menu-responsive__contenedor-boton").css("background-color", "#FFB40D");
             $('nav').animate({
                 left: '0'
             });
+            cambiarImagenjQ();
 
             contador = 0;
         } else {
             $('nav').animate({
                 left: '-100%'
             });
+            cambiarImagenjQ();
             contador = 1;
-            $(".contenedor-boton").css("background-color", "#000000a6");
+            //$(".menu-responsive__contenedor-boton").css("background-color", "#000000a6");
         }
     });
 };
